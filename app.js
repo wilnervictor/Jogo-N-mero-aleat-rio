712798f3e475b8142b7bc91ddb6066f18bad23df
Wilner Victor <wilnervictor214@gmail.com>
2025-08-20 10:52:42 -0300

    aquivo de voz adicionado

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+let listaDeNumerosSorteados = []
+let numeroLimet = 10
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+     if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 function exibirMensagemNaTela(){
@@ -31,7 +41,17 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimet + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length
+    if (quantidadeDeElementosNaLista == numeroLimet){
+       listaDeNumerosSorteados = []
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio()
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido)
+        return numeroEscolhido
+    }
 }
 
 function  limparCampo() {
